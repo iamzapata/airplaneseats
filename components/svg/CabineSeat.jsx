242d5bfc709556450f68fe5a07459df628c38824
Andres Zapata <me@azapata.io>
2020-03-30 23:32:58 -0500

    Create <CabineSeat /> component. Draw seats

---
+++ components/svg/CabineSeat.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const CabineSeat = ({ x, y }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="14"
+    viewBox="0 0 16 14"
+    fill="none"
+    x={x}
+    y={y}
+  >
+    <path
+      d="M1.90991 3.25217C1.90991 2.00867 3.27299 1 4.96958 1H10.8569C12.5535 1 13.9274 2.006 13.9274 3.25217V8.79454C13.9274 10.0354 12.5535 11.052 10.8569 11.052H4.96958C3.27299 11.052 1.90991 10.0354 1.90991 8.79454V3.25217Z"
+      fill="#79AEFF"
+    />
+    <path
+      d="M1.90991 3.25217C1.90991 2.00867 3.27299 1 4.96958 1H10.8569C12.5535 1 13.9274 2.006 13.9274 3.25217V8.79454C13.9274 10.0354 12.5535 11.052 10.8569 11.052H4.96958C3.27299 11.052 1.90991 10.0354 1.90991 8.79454V3.25217Z"
+      fill="#79AEFF"
+      stroke="black"
+      stroke-width="0.309556"
+      stroke-miterlimit="3.864"
+    />
+    <path d="M1 13V9.9767H14.9533V13H1Z" fill="#79AEFF" />
+    <path
+      d="M1 13V9.9767H14.9533V13H1Z"
+      fill="#79AEFF"
+      stroke="black"
+      stroke-width="0.309556"
+      stroke-miterlimit="3.864"
+    />
+  </svg>
+)
+
+export default CabineSeat
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import Layout from 'components/Layout'
 import AirplaneCabine from 'components/svg/AirplaneCabine'
+import CabineSeat from 'components/svg/CabineSeat'
 
-const Index = () => <Layout> <AirplaneCabine /> </Layout>
+const renderSeats = () => {
+  const rows = [...Array(30).keys()]
+  const cols = [...Array(10).keys()]
+  let x = 10
+  let y = 150
+
+  const seats = []
+
+  rows.forEach(row => {
+    cols.forEach(col => {
+      seats.push(<CabineSeat x={x} y={y} />)
+
+      if ([2, 6].includes(col)) {
+        x += 20
+      } else {
+        x += 16
+      }
+    })
+
+    x = 10
+    y += 20
+  })
+
+  return seats
+}
+
+const Index = () => (
+  <Layout>
+    <AirplaneCabine>{renderSeats()}</AirplaneCabine>
+  </Layout>
+)
 
 export default Index
